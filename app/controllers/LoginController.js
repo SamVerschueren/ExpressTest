45f8eeba71eff8cfaeb454099ba7873f463e6ba5
Sam Verschueren <sam.verschueren@gmail.com>
2014-12-13 14:01:07 +0100

    Added LoginController

---
+++ app/controllers/LoginController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * This method will check for the credentials provided
+ * by the user and will throw an error if the credentials or not
+ * valid or redirect to the home page if valid.
+ * 
+ * @param  Object req The request object.
+ * @param  Object res The response object.
+ */
+exports.login = function(req, res) {
+    req.checkBody('mail', 'Please provide a valid email address.').isEmail();
+
+    var errors = req.validationErrors() || [];
+
+    if(errors.length > 0) {
+        return res.status(400).json(errors);
+    }
+};
